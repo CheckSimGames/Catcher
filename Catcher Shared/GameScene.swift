57b9e09b7c66a8ab79aa6482dc87ba0b7c2cd1d8
Mark Szymczyk <mark@swiftdevjournal.com>
2022-11-10 15:32:00 -0500

    Center the player horizontally and place the player 1/8 off the bottom of the screen.

--- Catcher Shared/GameScene.swift
+++ Catcher Shared/GameScene.swift
@@ -12,6 +12,15 @@ class GameScene: SKScene {
     var player = Player()
     
     func setUpScene() {
+        placePlayer()
+    }
+    
+    func placePlayer() {
+        // Center the player horizontally and place it 1/8 off the bottom of the screen.
+        let startX = size.width / 2
+        let startY = size.height / 8
+        
+        player.sprite.node.position = CGPoint(x: startX, y: startY)
         player.sprite.node.entity = player
         addChild(player.sprite.node)
     }
