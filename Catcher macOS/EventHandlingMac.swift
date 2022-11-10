224b8b99876cedc59161366ef307f150afd6c508
Mark Szymczyk <mark@swiftdevjournal.com>
2022-11-10 15:52:25 -0500

    Add code to the Mac version to move the player with the left and right arrow keys.

--- Catcher macOS/EventHandlingMac.swift
+++ Catcher macOS/EventHandlingMac.swift
@@ -10,6 +10,21 @@ import SpriteKit
 
 extension GameScene {
 
+    override func keyDown(with event: NSEvent) {
+        let keyCode: UInt16 = event.keyCode
+        
+        switch keyCode {
+        case 0x7B:
+            // Left arrow key pressed. Move left
+            player.transform.translate(CGVector(dx: -10.0, dy: 0.0))
+        case 0x7C:
+            // Right arrow key pressed. Move right
+            player.transform.translate(CGVector(dx: 10.0, dy: 0.0))
+        default:
+            return
+        }
+    }
+    
     override func mouseDown(with event: NSEvent) {
 
     }
