3d61fcf986f1f505032b36348d08ea5f19eba29c
Mark Szymczyk <mark@swiftdevjournal.com>
2022-11-11 14:55:23 -0500

    Fill an initial version of the Food entity.

--- Catcher Shared/Food.swift
+++ Catcher Shared/Food.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import GameplayKit
+
+class Food: GKEntity {
+    let sprite = SpriteComponent()
+    
+    init(location: CGPoint) {
+        super.init()
+        sprite.node = SKSpriteNode(color: .white, size: CGSize(width: 32, height: 32))
+        sprite.node.position = location
+        addComponent(sprite)
+        
+        let physicsComponent = PhysicsComponent(physicsBody: SKPhysicsBody(rectangleOf: CGSize(width: sprite.node.size.width, height: sprite.node.size.height)))
+        physicsComponent.physicsBody.affectedByGravity = false
+        addComponent(physicsComponent)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
