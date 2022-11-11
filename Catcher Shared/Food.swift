71b209c98ef83c92d0c3c2e865ee5d33d8bde34c
Mark Szymczyk <mark@swiftdevjournal.com>
2022-11-11 15:20:20 -0500

    Add a transform component to the Food entity.

--- Catcher Shared/Food.swift
+++ Catcher Shared/Food.swift
@@ -10,12 +10,16 @@ import GameplayKit
 
 class Food: GKEntity {
     let sprite = SpriteComponent()
+    let transform = TransformComponent()
     
     init(location: CGPoint) {
         super.init()
         sprite.node = SKSpriteNode(color: .white, size: CGSize(width: 32, height: 32))
         sprite.node.position = location
         addComponent(sprite)
+        
+        addComponent(transform)
+        transform.set(node: sprite.node)
         
         let physicsComponent = PhysicsComponent(physicsBody: SKPhysicsBody(rectangleOf: CGSize(width: sprite.node.size.width, height: sprite.node.size.height)))
         physicsComponent.physicsBody.affectedByGravity = false
